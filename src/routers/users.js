75be72d804eb697638d32fd6846058a521b521b7
duythien0912 <duythien0912@gmail.com>
2018-02-13 19:41:03 +0700

    confirmation email

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Users from "../models/user";
 import parseErrors from "../utils/parseError";
+import sendConfirmationEmail from "../mailer";
 
 const router = express.Router();
 
@@ -8,9 +9,11 @@ router.post("/", (req, res) => {
   const { email, password } = req.body.user;
   const user = new Users({ email });
   user.setPassword(password);
+  user.setConfirmationToken();
   user
     .save()
     .then(userRecord => {
+      sendConfirmationEmail(userRecord);
       res.json({ user: userRecord.toAuthJson() });
     })
     .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
